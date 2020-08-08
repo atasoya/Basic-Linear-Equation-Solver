509ef272c440c406e3692aecf0985e32c1b50b34
atasoya <45434939+atasoya@users.noreply.github.com>
2020-08-09 00:39:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import modules as mod
 import maths
 import validation as valid
+import tkinter as tk
+
 """
 equation = "2a=4"  #      # 1     2X/8=10   # 2     8/2X=10
 
@@ -11,7 +13,7 @@ print("operator :  ",mod.findoperator(equation))
 print(mod.findindedexofthevariable(equation))
 
 print(mod.findtypeoftequation(equation))
-"""
+
 equation = input("Enter the equation :   ")
 if valid.checknumberafterequal(equation) == False or valid.checkequalsign(equation) == False or valid.checkoperator(equation) == False :
     print("wrong format")
@@ -22,3 +24,39 @@ else :
     else :
         print("as")
         print(maths.calculatetype2(mod.findlastnumber(equation),mod.findnumberbeforex(equation),mod.findnumberafterequal(equation),mod.findoperator(equation)))
+"""
+answer = ""
+
+window = tk.Tk()
+window.title("Linear")
+
+label = tk.Label(text="Enter The Equation")
+
+entry = tk.Entry()
+
+def myclick():
+    equation = entry.get()
+    if valid.checknumberafterequal(equation) == False or valid.checkequalsign(equation) == False or valid.checkoperator(equation) == False :
+        answer = "wrong format"
+    else :
+        if mod.findtypeoftequation(equation) == 1 :
+            answer = maths.calculatetype1(mod.findlastnumber(equation),mod.findnumberbeforex(equation),mod.findnumberafterequal(equation),mod.findoperator(equation))
+        else :
+            answer = maths.calculatetype2(mod.findlastnumber(equation),mod.findnumberbeforex(equation),mod.findnumberafterequal(equation),mod.findoperator(equation))
+    entry.delete(0,len(entry.get()))
+    entry.insert(0,answer)
+
+
+
+
+button = tk.Button(text="Calculate",command=myclick)
+window.geometry("200x100")
+window.configure(bg="#58FA82")
+
+label.pack()
+entry.pack()
+button.pack()
+
+
+
+window.mainloop()
