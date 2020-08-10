13326c73c1fbd9df1eb7a0f4c8b59b7e4e733e1d
atasoya <45434939+atasoya@users.noreply.github.com>
2020-08-10 13:22:44 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,28 +3,7 @@ import maths
 import validation as valid
 import tkinter as tk
 
-"""
-equation = "2a=4"  #      # 1     2X/8=10   # 2     8/2X=10
 
-print("after equal part :  ",mod.findnumberafterequal(equation))
-print("variable component :   ",mod.findnumberbeforex(equation))
-print("last number :   ",mod.findlastnumber(equation))
-print("operator :  ",mod.findoperator(equation))
-print(mod.findindedexofthevariable(equation))
-
-print(mod.findtypeoftequation(equation))
-
-equation = input("Enter the equation :   ")
-if valid.checknumberafterequal(equation) == False or valid.checkequalsign(equation) == False or valid.checkoperator(equation) == False :
-    print("wrong format")
-else :
-    if mod.findtypeoftequation(equation) == 1 :
-        print("sa")
-        print(maths.calculatetype1(mod.findlastnumber(equation),mod.findnumberbeforex(equation),mod.findnumberafterequal(equation),mod.findoperator(equation)))
-    else :
-        print("as")
-        print(maths.calculatetype2(mod.findlastnumber(equation),mod.findnumberbeforex(equation),mod.findnumberafterequal(equation),mod.findoperator(equation)))
-"""
 answer = ""
 
 window = tk.Tk()
